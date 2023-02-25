1e5a4d4fe00a546c2ebefc968bfdc6bae5f422f3
mahmudul hasan <mahmudulhasan@mahmuduls-MacBook-Pro.local>
2023-02-25 13:37:13 +0100

    frame has been created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Chart from "./Components/Chart";
+
+function App() {
+  return (
+    <div className="px-10">
+      <Chart></Chart>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Table.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <div className="overflow-x-auto w-full">
+      <table className="table w-full">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job</th>
+            <th>Favorite Color</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <td>
+              <div className="flex items-center space-x-3">
+                <div>
+                  <div className="font-bold">Hart Hagerty</div>
+                  <div className="text-sm opacity-50">United States</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Zemlak, Daniel and Leannon
+              <br />
+              <span className="badge badge-ghost badge-sm">
+                Desktop Support Technician
+              </span>
+            </td>
+            <td>Purple</td>
+            <th>
+              <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
